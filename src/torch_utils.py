090a7f582d2aa7d75224938ea54a3bd42f87dbac
Abdul Fatir <abdulfatirs@gmail.com>
2021-10-26 21:49:36 +0800

    Reformat and add license

--- src/torch_utils.py
+++ src/torch_utils.py
@@ -1,3 +1,16 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
 from typing import Tuple
 import torch
 import torch.nn as nn
@@ -38,14 +51,16 @@ def init_layers(module):
         nn.init.xavier_uniform_(module.weight)
         if module.bias is not None:
             nn.init.zeros_(module.bias)
-    elif (type(module) == nn.RNNCell
-          or type(module) == nn.RNN
-          or type(module) == nn.GRUCell
-          or type(module) == nn.GRU
-          or type(module) == nn.LSTMCell
-          or type(module) == nn.LSTM):
+    elif (
+        type(module) == nn.RNNCell
+        or type(module) == nn.RNN
+        or type(module) == nn.GRUCell
+        or type(module) == nn.GRU
+        or type(module) == nn.LSTMCell
+        or type(module) == nn.LSTM
+    ):
         for name, param in module.named_parameters():
-            if 'bias' in name:
+            if "bias" in name:
                 nn.init.zeros_(param)
             else:
                 nn.init.xavier_uniform_(param)
